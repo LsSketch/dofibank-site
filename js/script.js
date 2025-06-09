39d8a08695a35d268e467ca36639821f8250c201
ntlsstudiosDev <132174663+LsSketch@users.noreply.github.com>
2025-06-09 03:46:13 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const downloadButton = document.querySelector(".download-button");
+  const topButton = document.querySelector(".top-button");
+
+  if (downloadButton) {
+    downloadButton.addEventListener("click", function () {
+      window.location.href = "https://github.com/LsSketch/DofiBank-web/raw/refs/heads/main/DofiBank-assinado-v1.apk";
+    });
+  }
+
+  if (topButton) {
+    topButton.addEventListener("click", function () {
+      window.location.href = "https://github.com/LsSketch/DofiBank-web/raw/refs/heads/main/DofiBank-assinado-v1.apk";
+    });
+  }
+});
